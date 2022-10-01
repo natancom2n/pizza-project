392a228b9a38b2c7c71760ffad62f7ffbc41b89b
NATANAEL ALVES PINTO <natan@NATANAELs-MacBook-Pro.local>
2022-10-01 20:50:03 -0300

    config express and cors

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -4,6 +4,9 @@ const router = Router();
 
 router.get('/test', (req: Request, res: Response) => {
     return res.json({ name: 'Pizza-Man!!' })
+
+    //exection Error
+    // throw new Error('Error on make this request!!');
 });
 
 export { router };
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,28 @@
-import express, { Request, Response, NextFunction } from 'express';
+import express, { Request, Response, NextFunction, json } from 'express';
+import 'express-async-errors';
+import cors from 'cors';
+
 import { router } from './routes';
 
 const app = express();
 app.use(express.json());
+app.use(cors());
+
 app.use(router);
+//midwor
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+        //if is the instance type of Error:
+        return res.status(400).json({
+            error: err.message
+        });
+    }
+
+    //if isn`t instance type Error:
+    return res.status(500).json({
+        status: 'error',
+        message: 'Internal server error.'
+    });
+});
+
 app.listen(3333, () => console.log('server online!!!'));   
