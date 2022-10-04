aeb675ddd820164c5df0f46e5cc4848fe9a71557
NATANAEL ALVES PINTO <natan@NATANAELs-MacBook-Pro.local>
2022-10-04 17:51:18 -0300

    config JWT token - 70

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import { CreateUserController } from './controllers/user/CreateUserController';
- 
+import { AuthUserContoller } from './controllers/user/AuthUserContoller';
+
 const router = Router();
 
 
 //-- USER ROTES --
 //for create (cadastrar) an user use post
 router.post('/users', new CreateUserController().handle)
+router.post('/session', new AuthUserContoller().handle)
 
 export { router };
