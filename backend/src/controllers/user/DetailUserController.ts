e60a7cd6162ecbd195e1a024f15494d57a1011b0
NATANAEL ALVES PINTO <natan@NATANAELs-MacBook-Pro.local>
2022-10-06 11:09:33 -0300

    72 middlewares isAuthenticate concluid

--- backend/src/controllers/user/DetailUserController.ts
+++ backend/src/controllers/user/DetailUserController.ts
@@ -3,9 +3,15 @@ import { DetailUserService } from '../../services/user/DetailUserService'
 
 class DetailUserController {
     async handle(req: Request, res: Response) {
+
+        //here get the user id inside the Request, inside the express
+        //this is modificaded for me inside the /src/@types/express/index.d.ts and also has changed in tsconfig.json
+        const user_id = req.user_id;
+        // console.log("Id do user = ", user_id);
+
         const detailUserService = new DetailUserService();
 
-        const user = await detailUserService.execute();
+        const user = await detailUserService.execute(user_id);
 
         return res.json(user);
 
--- backend/src/services/user/DetailUserService.ts
+++ backend/src/services/user/DetailUserService.ts
@@ -1,9 +1,23 @@
 import prismaClient from "../../prisma";
 
 class DetailUserService {
-    async execute() {
-        return ({ ok: true })
+    async execute(user_id: string) {
+        //go in the data base search if ID exist
+        const user = await prismaClient.user.findFirst({
+            where: {
+                id: user_id
+            },
+            //choose which return you want
+            select: {
+                id: true,
+                name: true,
+                email: true
+            }
+        })
 
+
+        // return ({ ok: true })
+        return user;
     }
 }
 
