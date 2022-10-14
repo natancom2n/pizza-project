679c726bd92d01ced326486950b6b420bed38e8c
NATANAEL ALVES PINTO <natan@NATANAELs-MacBook-Pro.local>
2022-10-14 12:34:13 -0300

    81:add item in the order ready

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -18,6 +18,7 @@ import { ListByCategoryController } from './controllers/product/ListByCategoryCo
 //import orders
 import { CreateOrderController } from './controllers/order/CreateOrderController';
 import { RemoveOrderController } from './controllers/order/RemoveOrderController';
+import { AddItemController } from './controllers/order/AddItemController';
 
 //images uploads
 import uploadConfig from './config/multer';
@@ -47,6 +48,7 @@ router.get('/category/product', isAuthenticated, new ListByCategoryController().
 // ---- Orders -------
 router.post('/order', isAuthenticated, new CreateOrderController().handle)
 router.delete('/order', isAuthenticated, new RemoveOrderController().handle)
+router.post('/order/add', isAuthenticated, new AddItemController().handle)
 
 
 
