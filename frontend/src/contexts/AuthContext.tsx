a2c5412dba72a34580624bcb515b3f632d0ba6fa
NATANAEL ALVES PINTO <natan@NATANAELs-MacBook-Pro.local>
2022-10-27 16:34:39 -0300

    100 - Conluid login em cokies, token with API

--- frontend/src/contexts/AuthContext.tsx
+++ frontend/src/contexts/AuthContext.tsx
@@ -1,9 +1,13 @@
 import { createContext, ReactNode, useState } from 'react';
+import { destroyCookie, setCookie, parseCookies } from 'nookies';
+import Router from 'next/router'
+import { api } from '../services/apiCliente';
 
 type AuthContextData = {
     user: UserProps;
     isAuthenticated: boolean;
     signIn: (credentials: SignInProps) => Promise<void>;
+    signOut: () => void;
 }
 
 type UserProps = {
@@ -22,21 +26,62 @@ type AuthProviderProps = {
 
 export const AuthContext = createContext({} as AuthContextData)
 
+//function for log out the user
+export function signOut() {
+    try {
+        destroyCookie(undefined, '@pizzafast.token')
+        Router.push('/')
+    } catch {
+        console.log('Log out error')
+    }
+}
+
 export function AuthProvider({ children }: AuthProviderProps) {
 
     const [user, setUser] = useState<UserProps>()
     const isAuthenticated = !!user; //when the user is login is true, when not is false ( !! change de variable for boolen)
 
+    //recived the e-mail and password of inputs 
     async function signIn({ email, password }: SignInProps) {
         // alert('Clicou no Login')
-        console.log("Login: ", email)
-        console.log("senha", password)
+        // console.log("Login: ", email)
+        // console.log("senha", password)
+        try {
+            const response = await api.post('/session', {
+                email,
+                password
+
+            })
+            // console.log(response.data);
+            const { id, name, token } = response.data;
+
+            setCookie(undefined, '@pizzafast.token', token, {
+                //time expire the cokie
+                masAge: 60 * 60 * 24 * 30, // 1 month
+                path: "/" //what path cokie get access
+
+            })
+            setUser({
+                id,
+                name,
+                email
+            })
+
+            //sendo the token for next requests
+            api.defaults.headers['Authorization'] = `Bearer ${token}`
+
+            //redirect the user to /dashbord
+            Router.push('/dashboard')
+
+        } catch (err) {
+            console.log('Error of access ', err)
+        }
 
 
     }
 
     return (
-        <AuthContext.Provider value={{ user, isAuthenticated, signIn }}>
+        <AuthContext.Provider value={{ user, isAuthenticated, signIn, signOut }}>
             {children}
         </AuthContext.Provider>
     )
